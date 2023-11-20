a5029aaa52aa4fbe8e2dc314393b50346bcc3099
aryansingh034 <132141859+aryansingh034@users.noreply.github.com>
2023-11-20 09:40:52 +0530

    Add files via upload

---
+++ FirstApplet.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+import java.applet.*;
+public class FirstApplet extends Applet{
+    public void paint(Graphics gh){
+        gh.drawstring("welcome to my first applet",300,150);
+    }
+}
